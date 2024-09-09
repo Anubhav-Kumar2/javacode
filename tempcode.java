2260fbd3f4d256436a86a5ae47e5fcbcfb50d7ef
Anubhav-Kumar2 <145273899+Anubhav-Kumar2@users.noreply.github.com>
2024-09-09 15:43:53 +0530

    Create tempcode.java

---
+++ tempcode.java
@@ -0,0 +1,13 @@
+package conditional;
+import java.util.Scanner;
+ public class tempcode {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    int a=sc.nextInt();
+    String result;
+    result=(a%2==0 && a%3==0) ? "ans found":"ans not found";
+    System.out.println(result);
+    sc.close();
+    }
+}
